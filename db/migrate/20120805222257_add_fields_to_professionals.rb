61c2ffe1c2a18855dd42f7dfa625cdc7dedf21d4
Alex Biz <alex@alx.bz>
2012-08-06 13:26:32 -0400

    Migration Fix

--- db/migrate/20120805222257_add_fields_to_professionals.rb
+++ db/migrate/20120805222257_add_fields_to_professionals.rb
@@ -2,9 +2,8 @@ class AddFieldsToProfessionals < ActiveRecord::Migration
   def change
     add_column :professionals, :education, :string
     add_column :professionals, :about, :text
-    add_column :professionals, :first_name, :text
-    add_column :professionals, :last_name, :text
-    add_column :professionals, :title, :text
-    add_column :professionals, :phone, :text                
+    add_column :professionals, :first_name, :string
+    add_column :professionals, :last_name, :string
+    add_column :professionals, :phone, :string          
   end
 end
